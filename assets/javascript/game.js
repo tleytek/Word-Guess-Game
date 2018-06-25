6935ed980d7b0d28cabe24cf0d90ff927117e9c8
tleytek <lucidmat94@gmail.com>
2018-06-24 21:28:32 -0400

    Completed most of the javascript logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,20 @@
+//indexOf is a small revelation for me here, my problems were finding a way to go through every character in a string/array
+//and if a key stroke matched 1 character in that array then the if condition would be met. But I soon found you couldn't have a for loop to go
+//through every index as part of an if statement, like this:
+//
+//if( for (int i = 0; i < 5; i++) { } ) { }
+//
+//and this set me back for MANY hours until I found .indexOf, which is basically a for loop executed on an array, and its little AWSOME quirk
+//of returning -1 if the character wasn't found
+//
+//
+
 var wins = 0;
 var guessesLeft = 14;
 var incorrectLetters = [];
 var correctLetters = [];
-var words = [
-  "planck",
-  "cherry mx",
-  "zealios",
-  "dactyl",
-  "dactyl-manuform",
-  "iris",
-  "lets split",
-  "ergodox"
-];
-var currentWord = words[Math.floor(Math.random() * 8)];
+var words = ["planck", "nyquist", "zealios", "dactyl", "iris", "ergodox"];
+var currentWord = words[Math.floor(Math.random() * 7)];
 //correct letters are turned into underscores at the same length of the random word
 for (var i = 0; i < currentWord.length; i++) {
   correctLetters.push("_");
@@ -20,12 +22,43 @@ for (var i = 0; i < currentWord.length; i++) {
 
 //
 function guess(letterGuessed) {
-  guessesLeft--;
+  //Once again the indexOf -1 blows my mind.
+  //If it looks through and finds a letter that already exists in incorrectLetters then it will NOT produce a -1 value
+  if (incorrectLetters.indexOf(letterGuessed) !== -1) {
+  }
 
   //this -1 return value as part of the indexOf method was a game changer for my logic
   //the method will return -1 if the value/letter is not found in currentWord
-  if (currentWord.indexOf(letter) === -1) {
-    incorrectLetters.push(letter);
+  else if (currentWord.indexOf(letterGuessed) === -1) {
+    incorrectLetters.push(letterGuessed);
+    guessesLeft--;
+    //if a correct letter was guessed then
+  } else {
+    for (var i = 0; i < currentWord.length; i++) {
+      //this changes the underscore of correctLetters *AT* the spot it belongs in, aka where that letter is in currentWord
+      if (correctLetters[i] === letterGuessed) {
+      } else if (currentWord[i] === letterGuessed) {
+        correctLetters[i] = letterGuessed;
+        guessesLeft--;
+      }
+    }
+  }
+}
+
+function checkScore() {
+  //again the -1 of indexOf is a game changer for me
+  //checks to see of any underscores are left, meaning the full word was guessed correctly
+  if (correctLetters.indexOf("_") === -1) {
+    wins++;
+  } else if (guessesLeft === 0) {
+    guessesLeft = 13;
+    currentWord = words[Math.floor(Math.random() * 7)];
+  }
+}
+
+function picture() {
+  switch (currentWord) {
+    case "planck":
   }
 }
 
@@ -33,4 +66,9 @@ function guess(letterGuessed) {
 document.onkeyup = function(event) {
   var letterGuessed = event.key;
   guess(letterGuessed);
+  checkScore();
+  console.log(letterGuessed);
+  console.log(guessesLeft);
+  console.log(correctLetters);
+  console.log(incorrectLetters);
 };
